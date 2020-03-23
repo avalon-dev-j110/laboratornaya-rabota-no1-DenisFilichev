ed059edb4393eb9261d43baf13bd955d213eab9c
DenisFilichev <61866785+DenisFilichev@users.noreply.github.com>
2020-03-23 12:45:50 +0300

    Add files via upload

--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -19,13 +19,13 @@ public class Main {
         if (secondName == null)
         {
             if (middleName != null)
-            {
+            {   // Создание обекта, имеющего имя, фамилию и отчество
                 human = new Person(lastName, firstName, middleName);
                 System.out.println(human.getFullName());
                 System.out.println(human.getAddress());
             }
             else
-                {
+                {   // Создание объекта, имеющего только имя и фамилию
                     fullName = lastName + " " + firstName;
                     human = new Person(fullName);
                     System.out.println(human.getFullName());
@@ -33,9 +33,9 @@ public class Main {
                 }
         }
         else   
-            {   
-                char e = secondName.charAt(0);
-                firstName = firstName + " " + e + ".";
+            {   // Создание объекта, имеющего фамилию, первое имя и второе имя
+                char e = secondName.charAt(0); // берем первую букву второго имени
+                firstName = firstName + " " + e + "."; // присоединяем букву второго имени к первому имени
                 human = new Person(lastName, firstName);
                 System.out.println(human.getFullName());
                 System.out.println(human.getAddress());
