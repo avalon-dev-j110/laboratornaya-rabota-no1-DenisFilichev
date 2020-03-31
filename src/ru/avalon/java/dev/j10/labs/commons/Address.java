a0c7b6072bb88c427a8494de2440c5252b9aba53
Denis Filichev <61866785+DenisFilichev@users.noreply.github.com>
2020-03-31 15:02:09 +0300

    Add files via upload

--- src/ru/avalon/java/dev/j10/labs/commons/Address.java
+++ src/ru/avalon/java/dev/j10/labs/commons/Address.java
@@ -1,20 +1,3 @@
-package ru.avalon.java.dev.j10.labs.commons;
-
-public class Address {
-    private String city;
-    private String street;
-    private String house;
-    public Address (String city, String street, String house){
-        this.city = city;
-        this.street = street;
-        this.house = house;
-    }
-    
-    public String getAddress (){
-        return city + ", " + street + ", " + house;
-    }
-}
-
 /*
  * TODO(Студент): Создайте класс Address.
  *
