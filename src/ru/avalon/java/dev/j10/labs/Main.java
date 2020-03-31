15ef4b0333bb340cc1a480d39c4cca4a7b0482ab
Denis Filichev <61866785+DenisFilichev@users.noreply.github.com>
2020-03-31 15:01:14 +0300

    Add files via upload

--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -1,53 +1,16 @@
 package ru.avalon.java.dev.j10.labs;
 
-import ru.avalon.java.dev.j10.labs.models.*;
+import ru.avalon.java.dev.j10.labs.models.Person;
 
 public class Main {
-      
+
     /*
      * FIXME(Студент): Измените определение метода так, чтобы он стал точкой входа в приложение.
      */
-    public static void main(String[] args) {
-        
-        String lastName = "Smith";//Фамилия
-        String firstName = "John";//Имя
-        String middleName = null;//Отчество
-        String secondName = "Edvard";//Второе имя
-        String fullName;
-        Person human;
-        
-        if (secondName == null)
-        {
-            if (middleName != null)
-            {   // Создание обекта, имеющего имя, фамилию и отчество
-                human = new Person(lastName, firstName, middleName);
-                System.out.println(human.getFullName());
-                System.out.println(human.getAddress());
-            }
-            else
-                {   // Создание объекта, имеющего только имя и фамилию
-                    fullName = lastName + " " + firstName;
-                    human = new Person(fullName);
-                    System.out.println(human.getFullName());
-                    System.out.println(human.getAddress()); 
-                }
-        }
-        else   
-            {   // Создание объекта, имеющего фамилию, первое имя и второе имя
-                char e = secondName.charAt(0); // берем первую букву второго имени
-                firstName = firstName + " " + e + "."; // присоединяем букву второго имени к первому имени
-                human = new Person(lastName, firstName);
-                System.out.println(human.getFullName());
-                System.out.println(human.getAddress());
-            }
-        
-        /*String ivanovFirstName = "Иванов";
-        String ivanovLastName = "Иван";
-        String ivanovPatronymic = "Иванович";
-        String smithFirstMiddleName = "John Edvard";
-        String smithLastName = "Smith";*/
+    Main() {
 
-        
+        Person ivanov = null;
+        Person smith = null;
 
         /*
          * TODO(Студент): Создайте экземпляры класса 'Person'
@@ -60,7 +23,7 @@ public class Main {
          *    чтобы она адресовала объект типа 'Person'
          *    описывающий человека по имени 'John Edvard Smith'.
          */
-         
+
         /*
          * TODO(Студент): Создайте несколько строковых переменных:
          *
